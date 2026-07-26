c451d403be8228dbf7171c30254f1bdf47ed7674
Claudia Adorno <claudiaadornosrl@gmail.com>
2026-07-26 17:22:38 -0300

    update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker — Ventas Adorno
-const CACHE_VERSION = 'ventas-adorno-v38-header-unificado-kebab';
+const CACHE_VERSION = 'ventas-adorno-v38b-botones-icono';
 const CACHE_ASSETS = ['./', './index.html', './manifest.webmanifest',
   './manifest-alcorta.webmanifest', './manifest-unicenter.webmanifest', './manifest-oficina.webmanifest',
   './favicon.svg'];
