4cc63357b4a41a09570dbcdc6e2a172a21c1ef1d
Claudia Adorno <claudiaadornosrl@gmail.com>
2026-08-06 15:09:12 -0300

    update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker — Ventas Adorno
-const CACHE_VERSION = 'ventas-adorno-v55-cant-tickets';
+const CACHE_VERSION = 'ventas-adorno-v56-icono-pwa';
 const CACHE_ASSETS = ['./', './index.html', './manifest.webmanifest',
   './manifest-alcorta.webmanifest', './manifest-unicenter.webmanifest', './manifest-oficina.webmanifest',
   './favicon.svg'];
